4c7da565cdd69bacf0debbafd442ef6702e0dd56
Ann-MarieKemp <aedalenb@gmail.com>
2020-07-24 18:08:43 -0400

    style: switch from table to flexbox to display chart

--- src/components/table.js
+++ src/components/table.js
@@ -2,24 +2,23 @@ import React from "react"
 
 const Table = ({ debts, clearCheckboxes, handleSelect }) => {
   return (
-    <table id="table-format">
-      <thead>
-        <tr>
-          <th>
-            <input
-              className="checkbox"
-              type="checkbox"
-              onClick={clearCheckboxes}
-            />
-          </th>
-          <th>Creditor</th>
-          <th>First Name</th>
-          <th>LastName</th>
-          <th>Min Pay%</th>
-          <th>Balance</th>
-        </tr>
-      </thead>
-      <tbody>
+    <div id="table-format">
+      <div className="table-div">
+        <div className="tr-div">
+          <input
+            className="checkbox"
+            type="checkbox"
+            onClick={clearCheckboxes}
+          />
+
+          <div className="th-div">Creditor</div>
+          <div className="th-div">First Name</div>
+          <div className="th-div">LastName</div>
+          <div className="th-div">Min Pay%</div>
+          <div className="th-div">Balance</div>
+        </div>
+      </div>
+      <div className="tablebody-div">
         {debts.map((debt, idx) => {
           const {
             firstName,
@@ -31,27 +30,26 @@ const Table = ({ debts, clearCheckboxes, handleSelect }) => {
           } = debt
           return (
             <React.Fragment key={idx}>
-              <tr>
-                <td>
-                  <input
-                    className="checkbox"
-                    type="checkbox"
-                    checked={isChecked}
-                    name={idx}
-                    onChange={handleSelect}
-                  />
-                </td>
-                <td>{creditorName}</td>
-                <td>{firstName}</td>
-                <td>{lastName}</td>
-                <td>{minPaymentPercentage}</td>
-                <td>{balance}</td>
-              </tr>
+              <div className="subtr-div">
+                <input
+                  className="checkbox"
+                  type="checkbox"
+                  checked={isChecked}
+                  name={idx}
+                  onChange={handleSelect}
+                />
+
+                <div className="td-div">{creditorName}</div>
+                <div className="td-div">{firstName}</div>
+                <div className="td-div">{lastName}</div>
+                <div className="td-div">{minPaymentPercentage}</div>
+                <div className="td-div">{balance}</div>
+              </div>
             </React.Fragment>
           )
         })}
-      </tbody>
-    </table>
+      </div>
+    </div>
   )
 }
 
